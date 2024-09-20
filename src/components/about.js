e45f7d46c65ca2f4b9e644d36a31eced6cefc6da
negarin-nb <nnb.se21@gmail.com>
2024-09-20 15:09:46 +0330

    title add

--- src/components/about.js
+++ src/components/about.js
@@ -1,15 +1,11 @@
 import React from "react";
 import aboutImg from "../images/about.jpeg";
+import Title from "./title";
 
 export default function About() {
   return (
     <section className="section" id="about">
-      <div className="section-title">
-        <h2>
-          about <span>us</span>
-        </h2>
-      </div>
-
+      <Title title="about" subTitle="us" />
       <div className="section-center about-center">
         <div className="about-img">
           <img src={aboutImg} className="about-photo" alt="awesome beach" />
